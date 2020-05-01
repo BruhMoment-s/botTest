f3b9f71bbd7c5597287cbe9f336b20c5cabca6b4
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <62805149+BruhMoment-s@users.noreply.github.com>
2020-05-01 16:15:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,10 +9,11 @@ client.on('message', message => {
     if (message.content.substring(0,8) === '_update ') {
         let embedContent1 = message.content.substring(8,message.length);
        
-        let embedContent = embedContent1.replaceAll("|","\n");
+        let embedContent = embedContent1.replace("|","\n");
         const embed = {
             "title": "Xedved update",
             "color": 8359053,
+            "timestamp": new Date(),
             "footer": {
               "icon_url": "https://media.discordapp.net/attachments/703857786496483359/704681488402219038/xedved_thing33.png",
               "text": "Xedved update alert"
@@ -34,8 +35,9 @@ client.on('message', message => {
               }
             ]
           };
+          
          message.channel.send(message.guild.defaultRole.toString(),{ embed });
-
+        
      //   message.reply({embed});
   	}
 });
