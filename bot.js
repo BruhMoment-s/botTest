95fdf49e125a71e847f532be262010b5edbe808b
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <62805149+BruhMoment-s@users.noreply.github.com>
2020-05-01 13:59:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,14 +7,14 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content.substring(0,8) === '_update ') {
-        let embedContent = message.content.substring(8);
-        let embed = new Discord.MessageEmbed();
-        embed.setDescription(embedContent);
-        embed.setColor('#c9c9c9');
-        embed.setTitle("Embed Made" );
-        embed.setTimestamp();
+        let embedContent = message.content.substring(8,message.length);
+      //  let embed = new Discord.MessageEmbed();
+    //    embed.setDescription(embedContent);
+     //   embed.setColor('#c9c9c9');
+     //   embed.setTitle("Embed Made" );
+      //  embed.setTimestamp();
 
-        message.reply(embed);
+        message.reply(embedContent);
   	}
 });
 
