58ff7016901d45319ab4e93d28f9db9ed1af7ee6
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <62805149+BruhMoment-s@users.noreply.github.com>
2020-05-01 17:42:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('message', message => {
     if (message.content.substring(0,8) === '_update ') {
         let embedContent1 = message.content.substring(8,message.length);
        
-        let embedContent = embedContent1.replace("|","\n");
+        let embedContent = embedContent1.replaceAll("|","\n");
         const embed = {
             "title": "Xedved update",
             "color": 8359053,
