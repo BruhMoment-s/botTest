d8a903b331291ec8f9d837b376e3017b513c9a13
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <62805149+BruhMoment-s@users.noreply.github.com>
2020-05-06 16:31:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,7 +54,7 @@ client.on('message', message => {
          
      //   message.reply({embed});
     }
-    if (message.content.substring(0,9) === 'download') {
+    if (message.content.substring(0,9) === '_download') {
       let html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
       let doc = Jsoup.parse(html); 
       let text = doc.body().text(); // "An example link"
