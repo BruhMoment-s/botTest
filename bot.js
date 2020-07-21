692a0958f7b5f062a42c9638785ba8b49f8cc194
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <62805149+BruhMoment-s@users.noreply.github.com>
2020-07-21 19:37:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,11 @@ client.on('message', message => {
     
   let linkDownload;
   let messagelower = message.toLowerCase();
-  
+  if (message.member.hasPermission("ADMINISTRATOR"))
+  {
+    message.channel.send("Message recieved as: " + messagelower);
+    
+  }
   
     if (messagelower.substring(0,8) === '_update ') {
      
